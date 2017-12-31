14f9517c774915f4d5cff5c79a097c6ee1a2a67d
Carl Markham <doddsey65@gmail.com>
2017-12-31 14:09:11 +0000

    add devise with default controllers

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username,
+    presence: true,
+    uniqueness: {
+      case_sensitive: false
+    },
+    format: {
+      with: /^[a-zA-Z0-9_\.]*$/,
+      multiline: true,
+    }
+end
