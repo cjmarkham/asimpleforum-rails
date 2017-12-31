277a309002bf066763dd716edf9edcbafe7ec769
Carl Markham <doddsey65@gmail.com>
2017-12-31 14:13:57 +0000

    set explicitly allowed tags for sanitizer

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Asimpleforum
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.action_view.sanitized_allowed_tags = ['strong', 'em', 'a', 'u']
   end
 end
